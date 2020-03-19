8fdee247579c43a0aea875f025bc8985af359127
Layne666 <791270330@qq.com>
2020-03-19 22:24:24 +0800

    fix(list): resolved list sorting issues

--- view/nexmoe/list.php
+++ view/nexmoe/list.php
@@ -81,7 +81,7 @@ function file_ico($item){
 		<?php foreach((array)$items as $item):?>
 			<?php if(!empty($item['folder'])):?>
 
-		<li class="mdui-list-item mdui-ripple">
+		<li class="mdui-list-item mdui-ripple" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
 			<a href="<?php echo get_absolute_path($root.$path.rawurlencode($item['name']));?>">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons">folder_open</i>
@@ -92,7 +92,7 @@ function file_ico($item){
 		  	</a>
 		</li>
 			<?php else:?>
-		<li class="mdui-list-item file mdui-ripple">
+		<li class="mdui-list-item file mdui-ripple" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
 			<a href="<?php echo get_absolute_path($root.$path).rawurlencode($item['name']);?>" target="_blank">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons"><?php echo file_ico($item);?></i>
@@ -201,7 +201,7 @@ $(function(){
         $('li[data-sort]').sortElements(function (a, b) {
             let data_a = $(a).attr("data-sort-" + sort_type), data_b = $(b).attr("data-sort-" + sort_type);
             let rt = data_a.localeCompare(data_b, undefined, {numeric: true});
-            return (sort_order === "more") ? 0-rt : rt;
+            return (sort_order === "less") ? 0-rt : rt;
         });
 
         $(this).attr("data-order", sort_order_to).text("expand_" + sort_order_to);
